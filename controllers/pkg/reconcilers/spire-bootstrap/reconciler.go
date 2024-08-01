5982de77140fa566e6dbb63d7c6ae0b74da2bc19
PrimalPimmy <Prashant20.pm@gmail.com>
2024-08-02 02:07:26 +0530

    Spire-agent conf, vault testing
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- controllers/pkg/reconcilers/spire-bootstrap/reconciler.go
+++ controllers/pkg/reconcilers/spire-bootstrap/reconciler.go
@@ -150,7 +150,7 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		if strings.Contains(secret.GetName(), cl.Name) {
 			secret := secret
 			fmt.Println("TESTING IFFFFFFF")
-			vaultClient.StoreKubeconfig(secret, client, "secret/kubeconfigs"+cl.Name, cl.Name)
+			vaultClient.StoreKubeconfig(ctx, secret, client, "/kubeconfigs"+cl.Name, cl.Name)
 		}
 	}
 
